8739b216df66e6f4c8a9eb98853abb0b66ac943d
patw <patw.hi@gmail.com>
2018-05-13 02:17:41 +0800

    Link react-native-google-signin

--- android/app/src/main/java/me/patw/amplifyexample/MainApplication.java
+++ android/app/src/main/java/me/patw/amplifyexample/MainApplication.java
@@ -3,6 +3,7 @@ package me.patw.amplifyexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
           new FBSDKPackage(mCallbackManager)
       );
     }
